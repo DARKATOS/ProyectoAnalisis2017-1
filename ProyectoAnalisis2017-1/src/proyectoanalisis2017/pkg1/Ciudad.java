0826533a877b7598bff8c7f7ca4c1bb014eda9a8
gianca970226 <giancarlosfigueroa@hotmail.com>
2017-03-18 23:06:56 -0500

    hol

--- ProyectoAnalisis2017-1/src/proyectoanalisis2017/pkg1/Ciudad.java
+++ ProyectoAnalisis2017-1/src/proyectoanalisis2017/pkg1/Ciudad.java
@@ -11,27 +11,29 @@ import java.io.Serializable;
  *
  * @author Gianka
  */
-public class Ciudad implements Serializable{
-    private Componente [][] matrizCiudad;
+public class Ciudad implements Serializable {
+
+    private Componente[][] matrizCiudad;
     private int n;//indica la canidad de columnas 
     private int m; //indica la cantidad de filas 
-    private int anchoCampo;
-    private int altoCampo;
-    private int anchoCiudad;
-    private int largoCiudad;
-    
-    
+    private int anchoCampo;//indica el ancho del rectangulo de representacion de un componente
+    private int altoCampo;//indica el alto del rectangulo de representacion de un componente
+    private int anchoCiudad;//indica el ancho del area de la ciudad 
+    private int altoCiudad;//indica el alto del area de la ciudad
+    private int cantidadNodos;//contiene la cantidad de nodos que tiene la ciudad
+
     public Ciudad() {
     }
 
-    public Ciudad(Componente [][] matrizCiudad, int n, int m, int anchoCampo, int altoCampo) {
+    public Ciudad(Componente[][] matrizCiudad, int n, int m, int anchoCampo, int altoCampo) {
         this.matrizCiudad = matrizCiudad;
         this.n = n;
         this.m = m;
+        this.cantidadNodos = 0;
         this.anchoCampo = anchoCampo;
         this.altoCampo = altoCampo;
-        this.anchoCiudad=this.n*this.anchoCampo;
-        this.largoCiudad=this.altoCampo * this.n;
+        this.anchoCiudad = this.n * this.anchoCampo;
+        this.altoCiudad = this.altoCampo * this.n;
     }
 
     public int getAltoCampo() {
@@ -46,15 +48,164 @@ public class Ciudad implements Serializable{
         return anchoCiudad;
     }
 
-    public int getLargoCiudad() {
-        return largoCiudad;
+    public int getAltoCiudad() {
+        return altoCiudad;
     }
+
+    
 
     public Componente[][] getMatrizCiudad() {
         return matrizCiudad;
     }
+    /**
+     * Nos indica si el componente es una calle quqe validamos con el nombre 
+     * @param componente componente a verificar
+     * @return resutaldo true es calle,false no es calle
+     */
+    public Boolean esCalle(Componente componente) {
+        boolean resultado = false;
+        if (componente.getNombre().equals("1.1")) {
+            componente.setTipo(6);
+            resultado = true;
+        } else if (componente.getNombre().equals("1.2")) {
+            componente.setTipo(5);
+            resultado = true;
+        } else if (componente.getNombre().equals("2.1")) {
+            componente.setTipo(1);
+            resultado = true;
+        } else if (componente.getNombre().equals("2.2")) {
+            componente.setTipo(3);
+            resultado = true;
+        } else if (componente.getNombre().equals("2.3")) {
+            componente.setTipo(2);
+            resultado = true;
+        } else if (componente.getNombre().equals("2.4")) {
+            componente.setTipo(4);
+            resultado = true;
+        }
+        return resultado;
+    }
+    /**
+     * Nos indica si el componente es una carretera que validamos con el nombre 
+     * @param componente componente a validar
+     * @return true es carretera, false no es carretera
+     */
+    public Boolean esCarretera(Componente componente) {
+        boolean resultado = false;
+        if (componente.getNombre().equals("3.1")) {
+            componente.setTipo(6);
+            resultado = true;
+        } else if (componente.getNombre().equals("3.2")) {
+            componente.setTipo(5);
+            resultado = true;
+        } else if (componente.getNombre().equals("6.1")) {
+            componente.setTipo(1);
+            resultado = true;
+        } else if (componente.getNombre().equals("6.2")) {
+            componente.setTipo(3);
+            resultado = true;
+        } else if (componente.getNombre().equals("6.3")) {
+            componente.setTipo(2);
+            resultado = true;
+        } else if (componente.getNombre().equals("6.4")) {
+            componente.setTipo(4);
+            resultado = true;
+        }
+        return resultado;
+    }
+   /**
+    * Este metodo nos recorre toda la ciduad,marcando si es cruce,calle o carretera 
+    * ademas va asigandole si es es nodo el id del nodo que va tener en el grafo
+    */
+    public void actualizarCiudad() {
+        this.cantidadNodos = 0;
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < this.m; j++) {
+                if (this.matrizCiudad[i][j] != null) {
 
-    
+                    if (esCruce(this.matrizCiudad[i][j])) {
+                        matrizCiudad[i][j].setEsCruce(true);
+                        marcarNodo(matrizCiudad[i][j]);
+                    } else {
+                        if (esCalle(this.matrizCiudad[i][j])) {
+                            matrizCiudad[i][j].setEsCalle(true);
+                        } else if (esCarretera(this.matrizCiudad[i][j])) {
+                            matrizCiudad[i][j].setEsCarretera(true);
+                        }
+                        if (esViaCortada(i, j)) {
+                            marcarNodo(matrizCiudad[i][j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+ /**
+  * Analizamos si es l componente es via osea si es carretera,calle,o cruce
+  * @param componente componente a validar
+  * @return true es via,false no es via
+  */
+    private Boolean esVia(Componente componente) {
+        Boolean resultado = false;
+        if (esCalle(componente) || esCarretera(componente) || esCruce(componente)) {
+            resultado = true;
+        }
+        return resultado;
+    }
+    /**
+     * En este metodo validamos si es una via que no finaliza en un cruce o si esta al tope de la ciudad
+     * los componente que van a entrar es si son calle o carretera
+     * @param i indice de fila donde esta el componente a validar
+     * @param j indice de columna donde esta el componente a validar
+     * @return  true es via cortada, false no es via cortada
+     */
+    private Boolean esViaCortada(int i, int j) {
+        Boolean resultado = false;
+        int contador = 0;
+
+        if (j + 1 < m && this.matrizCiudad[i][j + 1] != null && esVia(this.matrizCiudad[i][j + 1])) {
+            contador++;
+        }
+
+        if (j - 1 >= 0 && this.matrizCiudad[i][j - 1] != null && esVia(this.matrizCiudad[i][j - 1])) {
+            contador++;
+        }
+
+        if (i + 1 < n && this.matrizCiudad[i + 1][j] != null && esVia(this.matrizCiudad[i + 1][j])) {
+            contador++;
+        }
+
+        if (i - 1 >= 0 && this.matrizCiudad[i - 1][j] != null && esVia(this.matrizCiudad[i - 1][j])) {
+            contador++;
+        }
+
+        if (contador == 1) {
+            resultado = true;
+        }
+        return resultado;
+    }
+    /**
+     * Al componente que va se un nodo le asigna un id e incrimente la cantidad de nodos 
+     * de la ciudads
+     * @param componente componente a marcar
+     */
+    private void marcarNodo(Componente componente) {
+        componente.setIdNodo(this.cantidadNodos);
+        this.cantidadNodos++;
+    }
+    /**
+     * Validamos si el componente es una cruce que tambiem lo comprobamos con el nombre 
+     * @param componente componente a verificar
+     * @return true es cruce, false no es cruce
+     */
+    private boolean esCruce(Componente componente) {
+        Boolean resultado = false;
+        if (componente.getNombre().substring(0, 2).equals("4.") || componente.getNombre().substring(0, 2).equals("5.")) {
+            resultado = true;
+        }
+        return resultado;
+
+    }
 
     public int getM() {
         return m;
@@ -63,8 +214,11 @@ public class Ciudad implements Serializable{
     public int getN() {
         return n;
     }
-    
-    
-}
 
-  
+    public int getCantidadNodos() {
+        return cantidadNodos;
+    }
+
+   
+
+}
--- ProyectoAnalisis2017-1/src/proyectoanalisis2017/pkg1/Componente.java
+++ ProyectoAnalisis2017-1/src/proyectoanalisis2017/pkg1/Componente.java
@@ -11,26 +11,82 @@ import java.io.Serializable;
  *
  * @author root
  */
-public class Componente implements Serializable{
-    private String tipo;
-    private String ruta;
+public class Componente implements Serializable {
+
+    private int tipo;//1 abajo-arriba;2arriba-abajo;3izq-Der;4der-izq;5doblehorizontal;6doblevertical
+    private String ruta; //nos indica donde esta la imagen para pintarla en el panel    
+    private Boolean esCruce;//nos indica si es cruce o no
+    private Boolean esCalle;//nos indica si es calle o no
+    private Boolean esCarretera;// nos indica si es carretera o no
+    private String nombre;// nos indica el nombre del compoente 
+    private int idNodo;//si es -1 no es nodo y >=0 si es un nodo y el valor es el identificador del nodo
 
     public Componente() {
     }
 
-    public Componente(String tipo) {
-        this.tipo = tipo;
-        this.ruta = "../ImgComponentes/"+this.tipo+".png";
+    public Componente(String nombre) {
+        this.nombre = nombre;
+        this.tipo = 0;
+        this.ruta = "../ImgComponentes/" + nombre + ".png";
+        this.idNodo = -1;
+        this.esCruce = false;
+        this.esCalle = false;
+        this.esCarretera = false;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEsCarretera(Boolean esCarretera) {
+        this.esCarretera = esCarretera;
+    }
+
+    public Boolean getEsCarretera() {
+        return esCarretera;
+    }
+
+    public void setEsCalle(Boolean esCalle) {
+        this.esCalle = esCalle;
+    }
+
+    public Boolean getEsCalle() {
+        return esCalle;
     }
 
     public String getRuta() {
         return ruta;
     }
 
-    public String getTipo() {
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getTipo() {
         return tipo;
     }
-    
-    
-    
+
+    public void setIdNodo(int idNodo) {
+        this.idNodo = idNodo;
+    }
+
+    public int getIdNodo() {
+        return idNodo;
+    }
+
+    public void setEsCruce(Boolean esCruce) {
+        this.esCruce = esCruce;
+    }
+
+    public Boolean getEsCruce() {
+        return esCruce;
+    }
+
+   
+
 }
